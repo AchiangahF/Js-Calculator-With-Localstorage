1890f26eaa899d7a873dc27a32613d674ca463db
Joel Achiangah Fombin <joelfombin@gmail.com>
2024-11-22 20:49:05 +0100

    multiplication and division operations fixed

--- index.js
+++ index.js
@@ -13,25 +13,40 @@ let ClearScreen = () =>{
     outScreen.innerText = "";
 }
 
+// function to delete element
+
+let delElement = () =>{
+    if(inScreen.innerText != ''){
+        let expression = inScreen.textContent;
+        inScreen.innerText = expression.slice(0, expression.length-1);
+    }
+}
+
 // evaluation and output function
 
 let Evalaute = (str) =>{
     try{
         if(str.includes('+') || str.includes('-') || str.includes('÷') || str.includes('×')){
-            let result = eval(str);
+
+            let SW_div = str.replaceAll('÷', '/');
+            let SW_mult = SW_div.replaceAll('×', '*');
+            let result = eval(SW_mult);
             outScreen.innerText = result;
+
         }
     }
     catch(e){
-        //console.log(`Error: ${e}`);
+        console.log(`Error: ${e}`);
+        outScreen.innerText = "";
     }
 }
 
 buttons.forEach(button => {
     button.addEventListener("click", (e)=>{
-        if(e.target.innerText == 'C'){
+        if(e.target.innerText == 'D'){
             // CALL THE DEL FUNCTION
-            ClearScreen();
+            delElement();
+            Evalaute(inScreen.innerText);
         }
         else{
             // Print To Screen
@@ -46,11 +61,13 @@ buttons.forEach(button => {
 // ---------------------------------- LOCAL STORAGE CONTROL MECHANISM --------------------------------------
 //----------------------------------------------------------------------------------------------------------
 
-let calculatorStorage = [];
+let calculatorStorage = [];             // This array initailizes the storage process
 
 const saveData = () =>{
-    let count = localStorage.length;
+    
     let dt = new Date();
+
+    // initializing the data
 
     let data = {
         date:`${dt.getDate()}-${dt.getMonth()}-${dt.getFullYear()}`,
@@ -58,10 +75,12 @@ const saveData = () =>{
         result:outScreen.innerText,
     };
 
+    // save for and empty history
     if(localStorage.length == 0){
         calculatorStorage.push(data);
         localStorage.setItem(`calStore`, JSON.stringify(calculatorStorage));
     }
+    // save for history that is not empty
     else{
         let oldStorage = JSON.parse(localStorage.getItem('calStore'));
         let newStorage = [data, ...oldStorage];
@@ -71,14 +90,20 @@ const saveData = () =>{
 }
 
 // Getting the storage control buttons from dom
+// and passing the saveData() as callback if save-button is click. 
 
 const controlBtns = document.querySelectorAll(".controlBtn");
 controlBtns.forEach(controlBtn => {
     controlBtn.addEventListener("click", (e)=>{
-        if(e.target.alt == 'save' && inScreen.innerText != ""){
+        if(e.target.alt == 'save' && outScreen.innerText != ""){
             console.log('true');
             saveData();                 // Saving is taking place
             console.log(JSON.parse(localStorage.getItem('calStore')));
+        }
+        else if(e.target.alt == 'reset'){
+            // clears the screen
+
+            ClearScreen();
         }
     });
 });
